76cf684fcf88a303837b58a43a4b020b9e805e22
RaiyanMatadar <raiyanmatadar001@gmail.com>
2025-12-31 14:19:41 +0530

    Moved 4-E-Commerce from RED-AND-WHITE Original folder to here

---
+++ javascript.js
@@ -0,0 +1,97 @@
+let data = [];
+
+// fetching APIs data 
+async function fetchData() {
+    try {
+        // we have fentch API here
+        const response = await fetch("https://fakestoreapi.com/products");
+        data = await response.json()
+        ShowDataFetchedData(data)
+
+    } catch (error) {
+        console.log("Sorry there is an error");
+    }
+}
+fetchData();
+
+// after fetch we have made product card for all the fetched data 
+const parent = document.querySelector('div');
+
+function ShowDataFetchedData(arr) {
+    parent.innerHTML = ""; // CLEAR OLD CARDS
+
+    arr.forEach((element) => {
+        const div = document.createElement("div");
+        let {
+            id,
+            image,
+            category,
+            description,
+            price,
+            rating: {
+                rate,
+                count
+            },
+            title
+        } = element
+
+        div.classList.add("box")
+        div.innerHTML = `
+            <img src="${image}" alt="">
+            <p>${category}</p>
+            <p>${price}</p>
+            <span>${rate}</span>
+            <p>${title}</p>
+            <button onclick="addtolocalstorage(${id})">Add to cart</button>
+            <button onclick="ProductsDetails(${id})">Products Details</button>            
+            `
+        parent.appendChild(div)
+    })
+}
+
+
+// this will take input then return the data based on input
+const search = document.getElementById('search');
+let debounceTimeout;
+
+search.addEventListener('input', (event) => {
+    clearTimeout(debounceTimeout);
+    debounceTimeout = setTimeout(() => {
+        let input = event.target.value.toLowerCase().trim()
+        let filtered = data.filter((element) => {
+            return element.title.toLowerCase().includes(input) ||
+                element.category.toLowerCase().includes(input);
+        });
+        ShowDataFetchedData(filtered)
+    }, 1000);
+})
+
+// it will check if the duplicate data in local storage exist 
+// say its already exist if not then add the product 
+function addtolocalstorage(id) {
+    let arr = []
+    arr.push(id)
+    let exitarray = JSON.parse(localStorage.getItem('mycart'))
+    console.log(exitarray);
+
+    if (exitarray && exitarray.includes(id)) {
+        alert("product allready added")
+        return
+    }
+
+    if (exitarray) {
+        exitarray = [...exitarray, ...arr]
+        localStorage.setItem("mycart", JSON.stringify(exitarray))
+    } else {
+        localStorage.setItem("mycart", JSON.stringify(arr))
+    }
+}
+
+// Add to Cart Product feture (this will select the products then add them into local storage)
+document.getElementById("viewcart").onclick = () => {
+    window.location.href = 'cart.html';
+}
+
+function ProductsDetails(id) {
+    window.location.href = `ProductsDetails.html?id=${id}`;
+}
---
+++ redme.md
@@ -0,0 +1,6 @@
+E commerce 
+
+this website has search bar
+- fetching API - we have fetch the API and made products card from the it
+- searching feature - we can search product using titles of the products  
+- add to cart - we have add to card product feture where we can add to cart and view the added products 
